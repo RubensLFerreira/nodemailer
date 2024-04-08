6c678a14776d52ceede7910db75ae5058d89102e
RubensLFerreira <rubenslima000@gmail.com>
2024-04-07 22:31:34 -0300

    refactor: organizing the methods

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import dotenv from "dotenv";
-import express from "express";
+import express, { Application } from "express";
 import path from "path";
-import * as nodemailer from "nodemailer";
-
+import nodemailer, { Transporter, SendMailOptions } from "nodemailer";
+import dotenv from "dotenv";
 dotenv.config();
 
-const server = express();
-
-class Main {
+interface EmailConfig {
   service: string;
   host: string;
   port: number;
@@ -19,63 +16,43 @@ class Main {
   to: string;
   subject: string;
   text: string;
+}
 
-  constructor(
-    service: string,
-    host: string,
-    port: number,
-    secure: boolean,
-    username: string,
-    password: string,
-    name: string,
-    address: string,
-    to: string,
-    subject: string,
-    text: string
-  ) {
-    this.service = service;
-    this.host = host;
-    this.port = port;
-    this.secure = secure;
-    this.username = username;
-    this.password = password;
-    this.name = name;
-    this.address = address;
-    this.to = to;
-    this.subject = subject;
-    this.text = text;
+class Main {
+  private config: EmailConfig;
+  private server: Application;
+
+  constructor(config: EmailConfig) {
+    this.config = config;
+    this.server = express();
   }
 
-  start() {
-    server.listen(3000, () => {
-      console.log("Server is running on port 3000");
-    });
+  listen() {
+    this.server.listen(3000, () => console.log("Server is running"));
   }
 
-  get transporter() {
-    const transporter = nodemailer.createTransport({
-      service: this.service,
-      host: this.host,
-      port: this.port,
-      secure: this.secure,
+  private createTransporter(): Transporter {
+    return nodemailer.createTransport({
+      service: this.config.service,
+      host: this.config.host,
+      port: this.config.port,
+      secure: this.config.secure,
       auth: {
-        user: this.username,
-        pass: this.password,
+        user: this.config.username,
+        pass: this.config.password,
       },
     });
-
-    return transporter;
   }
 
-  get mailOption() {
-    const mailOptions = {
+  private createMailOptions(): SendMailOptions {
+    return {
       from: {
-        name: this.name,
-        address: this.address,
+        name: this.config.name,
+        address: this.config.address,
       },
-      to: this.to,
-      subject: this.subject,
-      text: this.text,
+      to: this.config.to,
+      subject: this.config.subject,
+      text: this.config.text,
       attachments: [
         {
           filename: path.join(__dirname, "/confia.jpeg"),
@@ -83,35 +60,34 @@ class Main {
         },
       ],
     };
-
-    return mailOptions;
   }
 
-  async sendMail(
-    transporter: nodemailer.Transporter,
-    mailOptions: nodemailer.SendMailOptions
-  ) {
+  async sendMail() {
+    const transporter = this.createTransporter();
+    const mailOptions = this.createMailOptions();
+
     try {
       const info = await transporter.sendMail(mailOptions);
-      console.log("Message sent: %s", info.messageId);
+      console.log(`Message sent: ${info.messageId}`);
     } catch (error) {
       console.error("Error occurred: ", error);
     }
   }
 }
 
-const main = new Main(
-  process.env.SERVICE!,
-  process.env.HOST!,
-  Number(process.env.PORT),
-  Boolean(process.env.SECURE),
-  process.env.AUTH_USER!,
-  process.env.AUTH_PASS!,
-  process.env.FROM_NAME!,
-  process.env.FROM_ADDRESS!,
-  process.env.TO!,
-  process.env.SUBJECT!,
-  process.env.TEXT!
-);
+const emailConfig: EmailConfig = {
+  service: process.env.SERVICE!,
+  host: process.env.HOST!,
+  port: Number(process.env.PORT),
+  secure: Boolean(process.env.SECURE),
+  username: process.env.AUTH_USER!,
+  password: process.env.AUTH_PASS!,
+  name: process.env.FROM_NAME!,
+  address: process.env.FROM_ADDRESS!,
+  to: process.env.TO!,
+  subject: process.env.SUBJECT!,
+  text: process.env.TEXT!,
+};
 
-main.sendMail(main.transporter, main.mailOption);
+const main = new Main(emailConfig);
+main.sendMail();
